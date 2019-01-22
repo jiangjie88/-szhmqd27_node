02f9621a5ee9341864bc1a2d7e0d94d2398777db
jiangjie88 <1337200065@qq.com>
2019-01-22 13:06:31 +0800

    student分支-1

---
+++ src/controllers/studentManagerController.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const template = require("art-template");
+const databasetool = require(path.join(__dirname,"../tools/databasetool.js"))
+/**
+ * 返回列表页面
+ * @param {*} req
+ * @param {*} res
+ */
+const getStudentListPage = (req, res) => {
+  const keyword = req.query.keyword || ''
+
+  databasetool.findMany('studentInfo',{name:{$regex:keyword}},(err,docs)=>{
+    // 这个里面的代码，是当databasetool中findMany执行了callback
+    // callback中会把 err,docs传递过来
+    // 渲染页面的代码
+    const html = template(path.join(__dirname, "../public/views/list.html"), {students:docs,keyword});
+        
+    res.send(html);
+  })
+};
+
+module.exports = {
+  getStudentListPage
+};
